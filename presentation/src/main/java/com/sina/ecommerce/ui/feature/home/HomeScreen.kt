fe9d614cbbadb2181c4a4f7bf862163ed75a1a2f
sina <sina.tabriziyan@gmail.com>
2024-11-01 21:31:22 +0330

    fix header row

--- presentation/src/main/java/com/sina/ecommerce/ui/feature/home/HomeScreen.kt
+++ presentation/src/main/java/com/sina/ecommerce/ui/feature/home/HomeScreen.kt
@@ -1,14 +1,18 @@
 package com.sina.ecommerce.ui.feature.home
 
 import android.util.Log
+import android.widget.Space
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -26,10 +30,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
 import com.sina.domain.model.Product
+import com.sina.ecommerce.R
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -62,8 +68,44 @@ fun HomeScreen(
 }
 
 @Composable
+fun ProfileHeader() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 16.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = null,
+                modifier = Modifier.size(48.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Column {
+                Text(text = "Hello", style = MaterialTheme.typography.bodySmall)
+                Text(text = "John Doe", style = MaterialTheme.typography.bodySmall)
+            }
+        }
+        Image(
+            painter = painterResource(id = R.drawable.ic_notification),
+            contentDescription = null,
+            modifier = Modifier.align(Alignment.CenterEnd)
+        )
+    }
+}
+
+@Composable
 fun HomeContent(featured: List<Product>, popularProduct: List<Product>) {
     LazyColumn {
+        item {
+            Spacer(modifier = Modifier.size(8.dp))
+            ProfileHeader()
+            Spacer(modifier = Modifier.size(16.dp))
+
+        }
         item {
             if (featured.isNotEmpty()) {
                 HomeProductRow(title = "Featured", products = featured)
@@ -75,7 +117,6 @@ fun HomeContent(featured: List<Product>, popularProduct: List<Product>) {
         }
     }
 }
-
 
 @Composable
 fun HomeProductRow(title: String, products: List<Product>) {
